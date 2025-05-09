5da230f68c57a7b367592062a2c5a6651ca5e42d
Nipun Kalra <143874150+nipunkalraa@users.noreply.github.com>
2025-05-09 10:22:35 +0100

    Add files via upload

---
+++ Code_Predicting_Food_Consumption_using_ML.R
@@ -0,0 +1,11 @@
+# set working directory
+setwd("M:/Portfolio/My_Predictive/PRED_3")
+
+# Install required packages
+install.packages("tidyverse")
+install.packages("caret")
+install.packages("colorspace")
+
+library(tidyverse)
+library(caret)
+library(colorspace)
